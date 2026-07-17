81ec52e976323ee98e7357c15c02bfaaa99b57e7
Mayurraj Singh Rathore <mayurraj.singh@webcluesinfotech.com>
2026-07-17 16:32:30 +0530

    add

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,20 +6,29 @@ function Hero() {
     <section className="hero" id="home">
       <div className="container hero-box">
         <div className="hero-text">
-          <BlurText 
-            text="Premium Box Cricket Turf" 
-            className="title" 
-            delay={100} 
-            animateBy="words" 
-            direction="top" 
+          <BlurText
+            text="Premium Box Cricket Turf"
+            className="title"
+            delay={200}
+            animateBy="words"
+            direction="top"
           />
+
           <p className="text">
+            <BlurText
+              text='cricket is best'
+              className="title"
+              delay={200}
+              animateBy="words"
+              direction="top"
+
+            />
             Experience the best cricket turf in the city. Play day or night with
             professional lighting and high-quality artificial grass.
           </p>
           <a href="#booking-form" className="button">Book Your Slot</a>
         </div>
-        
+
         <div className="hero-image">
           <img src="/cric.png" alt="Premium Box Cricket Turf" className="image" />
         </div>
